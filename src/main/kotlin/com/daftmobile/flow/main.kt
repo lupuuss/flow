98f1959eb940ad333c48783e27dff53fc2a4bd13
lupuuss <damian.baczynski@gmail.com>
2022-04-10 16:52:24 +0200

    3. Flows are cancellable...

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 
 fun createFlow() = flow {
     emit(1)
@@ -15,10 +16,12 @@ fun createFlow() = flow {
     emit(3)
 }
 fun main() =  runBlocking {
-    createFlow()
-        .noisyFilter { it % 2 != 0 }
-        .noisyMap { "String($it)" }
-        .collect { println("Collect $it") }
+    withTimeout(750) {
+        createFlow()
+            .noisyFilter { it % 2 != 0 }
+            .noisyMap { "String($it)" }
+            .collect { println("Collect $it") }
+    }
 }
 
 fun <T, R> Flow<T>.noisyMap(transform: suspend (T) -> R): Flow<R> = map {
