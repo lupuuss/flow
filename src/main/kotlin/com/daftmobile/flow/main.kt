7ce93db5a22bca449ca88174a2b230991a1fc44d
lupuuss <damian.baczynski@gmail.com>
2022-04-10 17:11:08 +0200

    3.2 Make busy flow cancellable again!

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -1,14 +1,17 @@
 package com.daftmobile.flow
 
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 
 fun createFlow() = flowOf(1, 2, 3, 4) // or listOf(1, 2, 3, 4).asFlow()
 
-fun main() =  runBlocking {
-    createFlow().collect {
-        if (it == 2) cancel()
-        println(it)
-    }
+fun main() = runBlocking {
+    createFlow()
+        .cancellable()
+        .collect {
+            if (it == 2) cancel()
+            println(it)
+        }
 }
