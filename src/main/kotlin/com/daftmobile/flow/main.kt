57b85871b5dfab57a1dfb3155afe6a5c034c6491
lupuuss <damian.baczynski@gmail.com>
2022-04-10 16:57:08 +0200

    3.1 ...until they aren't

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -1,35 +1,14 @@
 package com.daftmobile.flow
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
 
-fun createFlow() = flow {
-    emit(1)
-    delay(500)
-    emit(2)
-    delay(500)
-    emit(3)
-}
+fun createFlow() = flowOf(1, 2, 3, 4) // or listOf(1, 2, 3, 4).asFlow()
+
 fun main() =  runBlocking {
-    withTimeout(750) {
-        createFlow()
-            .noisyFilter { it % 2 != 0 }
-            .noisyMap { "String($it)" }
-            .collect { println("Collect $it") }
+    createFlow().collect {
+        if (it == 2) cancel()
+        println(it)
     }
-}
-
-fun <T, R> Flow<T>.noisyMap(transform: suspend (T) -> R): Flow<R> = map {
-    println("Map: $it")
-    transform(it)
-}
-
-fun <T> Flow<T>.noisyFilter(predicate: (T) -> Boolean): Flow<T> = filter {
-    println("Filter: $it")
-    predicate(it)
 }
