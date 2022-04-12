c47885ad2efc3fcf39ad63c43268ef218c54dfa2
lupuuss <damian.baczynski@gmail.com>
2022-04-12 22:21:27 +0200

    4.1 ...must be preserved

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -2,17 +2,22 @@ package com.daftmobile.flow
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 
 fun createFlow() = flow {
     emit(1)
     delay(500)
-    emit(2)
+    runCatching { emit(2) }.onFailure { println("Caught from emit: $it") }
     delay(500)
-    throw IllegalStateException("Boom!")
+    emit(3)
 }
+
 fun main() = runBlocking {
-    val flow = createFlow()
+    val flow = createFlow().map {
+        if (it == 2) throw IllegalStateException("Boom!")
+        else "$it"
+    }
     try {
         flow.collect {
             println(it)
