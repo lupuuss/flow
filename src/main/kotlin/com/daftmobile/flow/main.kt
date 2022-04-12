c0e8ae7de673359d258c4811c1f7b879df63107a
lupuuss <damian.baczynski@gmail.com>
2022-04-12 23:49:08 +0200

    5. Intermediate operators!

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -1,26 +1,40 @@
+@file:OptIn(FlowPreview::class)
+@file:Suppress("unused")
+
 package com.daftmobile.flow
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 
 fun createFlow() = flow {
-    emit(1)
-    delay(500)
-    emit(2)
-    delay(500)
-    throw IllegalStateException("Boom!")
+    var i = 0
+    while (true) emit(i++)
 }
 
+fun flow1() = createFlow()
+    .drop(1)
+    .take(10)
+    .filterNot { it % 2 == 0 }
+    .map { "+".repeat(it) }
+
+fun flow2() = emptyFlow<Int>()
+    .onEmpty { emit(1) }
+    .onStart { emit(2) }
+    .onEmpty { emit(7) }
+    .onCompletion { emit(3) }
+
+fun flow3() = createFlow()
+    .drop(1)
+    .takeWhile { it % 100 != 0 }
+    .map { it.coerceAtMost(3) }
+    .distinctUntilChanged()
+
+fun flow4() = createFlow()
+    .sample(1_000)
+    .transform { if (it % 3 == 0) error("Boom!") else emit(it) }
+    .retry(retries = 2) { it is IllegalStateException }
+
 fun main() = runBlocking {
-    createFlow()
-        .catch {
-            emit(3)                        // We can emit fallback/default here...
-            println("Logged exception: $it")     // ..or log it
-            throw Exception("Rethrowing...", it) // ...or rethrow it!
-        }
-        .collect {
-            println(it)
-        }
+    flow1().collect { println(it) }
 }
