2db8dd9f9fe6edd0197f8f42d359d43196f6f516
lupuuss <damian.baczynski@gmail.com>
2022-04-16 23:26:13 +0200

    6. Terminal operators!

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -8,33 +8,71 @@ import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 
 fun createFlow() = flow {
-    var i = 0
-    while (true) emit(i++)
+    try {
+       for (i in 3..7) {
+           println("Emitting: $i")
+           emit(i)
+       }
+    } finally {
+        println("I am out!")
+    }
 }
 
-fun flow1() = createFlow()
-    .drop(1)
-    .take(10)
-    .filterNot { it % 2 == 0 }
-    .map { "+".repeat(it) }
-
-fun flow2() = emptyFlow<Int>()
-    .onEmpty { emit(1) }
-    .onStart { emit(2) }
-    .onEmpty { emit(7) }
-    .onCompletion { emit(3) }
-
-fun flow3() = createFlow()
-    .drop(1)
-    .takeWhile { it % 100 != 0 }
-    .map { it.coerceAtMost(3) }
-    .distinctUntilChanged()
-
-fun flow4() = createFlow()
-    .sample(1_000)
-    .transform { if (it % 3 == 0) error("Boom!") else emit(it) }
-    .retry(retries = 2) { it is IllegalStateException }
-
 fun main() = runBlocking {
-    flow1().collect { println(it) }
+
+    log("collect") {
+        createFlow().collect { println("Collecting: $it") }
+    }
+
+    log("collectIndexed") {
+        createFlow().collectIndexed { index, value -> println("Collecting: Index=$index Value=$value") }
+    }
+
+    log("first") {
+        createFlow().first()
+    }
+
+    log("lastOrNull") {
+        createFlow().lastOrNull()
+    }
+
+    log("single") {
+        createFlow().single()
+    }
+
+    log("reduce(Int::times)") {
+        createFlow().reduce(Int::times) // or .reduce { acc, value -> acc * value }
+    }
+
+    log("fold(0, Int::plus)") {
+        createFlow().fold(0, Int::plus)
+    }
+
+    log("runningReduce(Int::times)") {
+        createFlow()
+            .runningReduce(Int::times)
+            .collect { println("Running reduce collect: $it") }
+    }
+
+    log("scan/runningFold(0, Int::plus)") {
+        createFlow()
+            .scan(0, Int::plus) // alias for .runningFold(0, Int::plus)
+            .collect { println("Scan collect: $it") }
+    }
+
+    log("count { it % 2 == 0 }") {
+        createFlow().count { it % 2 == 0 }
+    }
+
+    log("toList") {
+        createFlow().toList()
+    }
+}
+
+inline fun <T> log(tag: String, block: () -> T) {
+    println("--- $tag ---")
+    runCatching { block() }
+        .onSuccess { println("--- $tag ==> $it ---") }
+        .onFailure { println("--- $tag ==> $it ---") }
+    println()
 }
