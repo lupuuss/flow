b8af0178d50b65b17fbbb7ace1c6d8d5dfbb950e
lupuuss <damian.baczynski@gmail.com>
2022-04-18 20:30:58 +0200

    12. Zip

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -8,21 +8,20 @@ import kotlinx.coroutines.flow.*
 import kotlin.time.ExperimentalTime
 
 fun divisorsOf(number: Int): Flow<Int> = flow {
-    log("Divisors of $number flow!")
     emit(1)
     for (i in 2..(number / 2)) if (number % i == 0) emit(i)
     if (number != 1) emit(number)
 }
 
 fun main() = runBlocking {
-    val divisors = List(10) { divisorsOf(it + 1) }
-    combine(flows = divisors, transform = Array<*>::contentToString)
-        .flowOn(Dispatchers.Default)
+    val div8 = divisorsOf(8)
+    val div12 = divisorsOf(12)
+
+    println("Left:  " + div8.toList().prettyToString())
+    println("Right: " + div12.toList().prettyToString())
+
+    div8.zip(div12) { left, right -> left to right }
         .collect(::println)
 }
 
-suspend fun log(msg: Any? = null) {
-    val context = currentCoroutineContext()
-    val msgText = msg?.let { ": $it" }.orEmpty()
-    println("${context.job} ${context[CoroutineDispatcher]}$msgText")
-}
+fun List<*>.prettyToString() = joinToString(separator = " | ") { it.toString().padStart(2, ' ') }
