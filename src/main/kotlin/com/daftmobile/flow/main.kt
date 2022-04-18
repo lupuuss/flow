45fd920d008bd8d7163edfcf996805c0a7cda830
lupuuss <damian.baczynski@gmail.com>
2022-04-18 18:00:35 +0200

    10. flattenConcat

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -4,27 +4,30 @@
 package com.daftmobile.flow
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
-fun createFlow() = flow {
-    for (i in 1..10) {
-        log("Emit: $i")
-        emit(i)
-        delay(100)
+fun createFlow() = flow { for (i in 1..10) emit(i) }
+
+fun divisorsOf(number: Int): Flow<Int> = flow {
+    log("Divisors of $number:")
+    emit(1)
+    for (i in 2..(number / 2)) {
+        if (number % i == 0) {
+            delay(20)
+            emit(i)
+        }
     }
+    if (number != 1) emit(number)
 }
 
 fun main() = runBlocking {
     val time = measureTime {
         createFlow()
-            .collectLatest {
-                log("Collecting: $it")
-                delay(300)
-                log("Collected: $it")
-            }
+            .map(::divisorsOf)   // or just .flatMapConcat(::divisorsOf)
+            .flattenConcat()
+            .collect(::println)
     }
     println("Time: $time")
 }
