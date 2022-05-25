5a7602bec558cebd1e3917954d82606c8fe16a09
lupuuss <damian.baczynski@gmail.com>
2022-05-25 23:32:41 +0200

    Recap

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -1,38 +1,37 @@
-@file:OptIn(FlowPreview::class, ExperimentalStdlibApi::class, ExperimentalTime::class, ExperimentalCoroutinesApi::class)
 @file:Suppress("unused")
 
 package com.daftmobile.flow
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
-import kotlin.time.ExperimentalTime
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+
+fun createFlow() = flow {
+    var i = 0
+    while(true) {
+        println("Emit $i")
+        emit(i++)
+    }
+}
 
 fun main() = runBlocking {
-    val flow = MutableStateFlow(1)
+    val flow1 = createFlow()
+        .transform { if (it < 5) emit(it) else throw IllegalStateException("Boom!") }
+        .map { "#$it" }
+        .catch { throw Exception("Caught exception $it") }
 
-    flow.value = 2
+    try {
+        println("Collect: ")
+        flow1.collect { println(it) }
+    } catch (e: Throwable) {
+        println("Error: $e")
+    }
+    println("Done!")
 
-    delay(300)
+    println("------------------------")
 
-    launchDelayed(100) { flow.value = 3 }
-    launchDelayed(150) { flow.value = 3 }
-    launchDelayed(200) { flow.value = 4 }
+    val flow2 = createFlow().onEach { delay(100) }
 
-    withTimeoutOrNull(300) { flow.simulateSubscriberIn("Alpha", this) }
-
-    withTimeoutOrNull(300) { flow.simulateSubscriberIn("Beta", this) }
-
-    cancel()
-}
-
-fun <T> SharedFlow<T>.simulateSubscriberIn(tag: String, scope: CoroutineScope): Job {
-    return onEach { println("$tag collect $it") }
-        .onStart { println("$tag subscription start") }
-        .onCompletion { println("$tag subscription end") }
-        .launchIn(scope)
-}
-
-fun CoroutineScope.launchDelayed(delayMs: Long, block: suspend () -> Unit) = launch {
-    delay(delayMs)
-    block()
+    withTimeout(1000) { flow2.collect() }
 }
