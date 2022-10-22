5531f3238132b4594015e79931bce3db1a3358e2
lupuuss <damian.baczynski@gmail.com>
2022-10-22 22:57:41 +0200

    13.3 Custom SharingStarted

--- src/main/kotlin/com/daftmobile/flow/main.kt
+++ src/main/kotlin/com/daftmobile/flow/main.kt
@@ -2,36 +2,42 @@
 
 package com.daftmobile.flow
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
 
-fun createFlow() = flow {
-    var i = 0
-    while(true) {
-        println("Emit $i")
+fun ticker(interval: Long): Flow<Long> = flow {
+    var i = 0L
+    while (true) {
         emit(i++)
+        delay(interval)
     }
 }
 
 fun main() = runBlocking {
-    val flow1 = createFlow()
-        .transform { if (it < 5) emit(it) else throw IllegalStateException("Boom!") }
-        .map { "#$it" }
-        .catch { throw Exception("Caught exception $it") }
+    val sharingCommands = MutableStateFlow(SharingCommand.START)
+    val flow = ticker(200)
+        .onEach { println("Emit: $it") }
+        .shareIn(this, started = { sharingCommands }, replay = 0)
 
-    try {
-        println("Collect: ")
-        flow1.collect { println(it) }
-    } catch (e: Throwable) {
-        println("Error: $e")
-    }
-    println("Done!")
+    println("Creating flow...")
 
-    println("------------------------")
+    delay(300)
 
-    val flow2 = createFlow().onEach { delay(100) }
+    withTimeoutOrNull(300) { flow.simulateSubscriberIn("Alpha", this) }
 
-    withTimeout(1000) { flow2.collect() }
+    sharingCommands.value = SharingCommand.STOP
+    delay(300)
+
+    withTimeoutOrNull(300) { flow.simulateSubscriberIn("Beta", this) }
+
+    delay(300)
+
+    cancel()
+}
+
+fun <T> SharedFlow<T>.simulateSubscriberIn(tag: String, scope: CoroutineScope): Job {
+    return onEach { println("$tag collect $it") }
+        .onStart { println("$tag subscription start") }
+        .onCompletion { println("$tag subscription end") }
+        .launchIn(scope)
 }
